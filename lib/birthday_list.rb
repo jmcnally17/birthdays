9902de694fdc5ac2cb29acf59525de6448ba9da5
Joe McNally <joe.t.mcnally@hotmail.com>
2022-03-25 11:07:37 +0000

    Created and passed the test for store an instance of the Birthday class inside the array of an instance of the BirthdayList class.

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,7 +1,13 @@
+require_relative '../lib/birthday'
+
 class BirthdayList
   attr_reader :array
 
   def initialize
     @array = []
+  end
+
+  def store(person)
+    @array.push(person)
   end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,7 +1,15 @@
 require_relative '../lib/birthday_list'
 
 describe BirthdayList do
+  let(:list) {BirthdayList.new}
+  let(:person1) {Birthday.new("Wayne Rooney", "1985-10-24")}
+
   it 'has an array to store birthdays in' do
-    expect(BirthdayList.new.array).to eq []
+    expect(list.array).to eq []
+  end
+
+  it 'can store a birthday in its array' do
+    list.store(person1)
+    expect(list.array[0]).to eq person1
   end
 end
